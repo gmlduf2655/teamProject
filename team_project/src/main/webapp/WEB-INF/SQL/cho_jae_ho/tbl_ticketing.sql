a37ec6a75a763f1df5b769406ae9e48912087db2
KH302 <KH302@DESKTOP-SBH8F2L>
2022-06-02 10:26:26 +0900

    조재호 1.ticketingVo -

--- team_project/src/main/webapp/WEB-INF/SQL/cho_jae_ho/tbl_ticketing.sql
+++ team_project/src/main/webapp/WEB-INF/SQL/cho_jae_ho/tbl_ticketing.sql
@@ -8,4 +8,19 @@ create table tbl_ticketing(
 	ticketingdate date
 );
 
+ALTER TABLE TBL_TICKETING  
+MODIFY (TICKETINGDATE DEFAULT sysdate );
+
+alter table tbl_ticketing
+add userid references tbl_user(userid);
+
+alter table tbl_ticketing
+modify (userid not null);
+
+alter table tbl_ticketing
+add ticketstatus char(1) check(ticketstatus in('1', '0'));
+
+alter table tbl_ticketing
+add canceldate date;
+
 create sequence seq_ticketing_ticketingno;
