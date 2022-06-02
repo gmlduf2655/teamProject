d05f6fbf7f00bde94b6e4e536672d09ddcc87d10
KH302 <KH302@DESKTOP-SBH8F2L>
2022-06-02 17:04:31 +0900

    조재호 1. cinemaVo, Dao, Test 완료

--- team_project/src/test/java/com/kh/team/CinemaDaoTest.java
+++ team_project/src/test/java/com/kh/team/CinemaDaoTest.java
@@ -1,6 +1,7 @@
 package com.kh.team;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,15 +18,57 @@ public class CinemaDaoTest {
 
 	@Autowired
 	private CinemaDao cinemaDao;
-	
+
 	@Test
 	public void cinemaDaoTest() {
+
+		CinemaVo cinemaVo = null;
+		boolean result = false;
 		
-		CinemaVo cinemaVo = new CinemaVo("영화관이름", "영화관주소", 50, "1", null, null);
-		boolean result = cinemaDao.insertCinema(cinemaVo);
+		String cinema_name		= "영화관 이름";
+		String cinema_address	= "영화관 주소";
+		int cinema_seat_count	= 500;
+		String cinema_status	= "0";
+		Date cinema_begin_date	= null;
+		Date cinema_finish_date	= null;
+		Date chgdate			= null;
 		
+		for (int i = 1; i <= 100; i++) {
+			cinemaVo = new CinemaVo(cinema_name, cinema_address, cinema_seat_count, cinema_status, cinema_begin_date, cinema_finish_date, chgdate);
+			result = cinemaDao.insertCinema(cinemaVo);
+		}
+
 		System.out.println("insertCinema result : " + result);
-		
-		
+
+	}
+	
+	@Test
+	public void updateCinemaInfoTest() {
+		CinemaVo cinemaVo = new CinemaVo(1, "CGV 울산 남구점", "울산광역시 남구", 100, "1", null, null, null, null);
+		boolean result = cinemaDao.updateCinemaInfo(cinemaVo);
+		System.out.println("updateCinemaInfoTest result : " + result);
+	}
+	
+	@Test
+	public void getCinemaListTest() {
+		List<CinemaVo> cinemaList = cinemaDao.getCinemaList("cinema_no", "desc");
+		for (CinemaVo vo : cinemaList) {
+			System.out.println("CinemaList : " + vo);
+		}
+	}
+	
+	@Test
+	public void getCinemaInfoTest() {
+		int cinema_no = 1;
+		CinemaVo cinemaVo = cinemaDao.getCinemaInfo(cinema_no);
+		System.out.println("cinemaVo : " + cinemaVo);
+	}
+	
+	@Test
+	public void updateCinemaStatusTest() {
+		int cinema_no			= 1;
+		String cinema_status	= "0";
+		boolean result = cinemaDao.updateCinemaStatus(cinema_no, cinema_status);
+		System.out.println("updateCinemaStatus result : " + result);
 	}
 }
