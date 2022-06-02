1934c427f1b0ad6bc02a5e8e9f89ac4a0296d2d4
Uttrue <andy1324@naver.com>
2022-06-02 14:47:44 +0900

    유수연viewPage

---
+++ team_project/src/main/java/com/kh/team/controller/MovieCrawlerController.java
@@ -0,0 +1,17 @@
+package com.kh.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/crawling")
+public class MovieCrawlerController {
+
+	@RequestMapping(value="/dbSave", method = RequestMethod.GET)
+	public String movieCrawling() {
+		System.out.println("dbSave");
+		
+		return "movie/movieList";
+	}
+}
