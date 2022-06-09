7fe92d8fa732bdfb1ddce0984509be293f3b2693
KH302 <KH302@DESKTOP-SBH8F2L>
2022-06-09 09:27:57 +0900

    조재호

--- team_project/src/main/java/com/kh/team/controller/MkCinemaController.java
+++ team_project/src/main/java/com/kh/team/controller/MkCinemaController.java
@@ -1,34 +1,31 @@
 package com.kh.team.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.RequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.kh.team.dao.CinemaDao;
-import com.kh.team.dao.CinemaRoomDao;
-import com.kh.team.dao.RoomSeatDao;
-import com.kh.team.dao.RoomTimelineDao;
+import com.kh.team.service.CinemaService;
 
 @Controller
 @RequestMapping("/mkcinema")
 public class MkCinemaController {
 	
 	@Autowired
-	private CinemaDao cinemaDao;
+	private CinemaService cinemaService;
 	
-	@Autowired
-	private CinemaRoomDao cinemaRoomDao;
-	
-	@Autowired
-	private RoomSeatDao roomSeatDao;
-	
-	@Autowired
-	private RoomTimelineDao roomTimelineDao;
-	
-	@RequestMapping(value = "/cinemaDB", method = RequestMethod.GET)
-	public String createCinema() {
+	@RequestMapping(value = "/cinema", method = RequestMethod.GET)
+	public String createCinema(HttpServletRequest request, Model model, RedirectAttributes rttr) {
+		String uri = request.getRequestURI();
+		int index = uri.indexOf("/");
+		String str = uri.substring(index);
+		System.out.println(str);
+		model.addAttribute("pageInfo", str);
 		return "cinema/cinemaDB";
 	}
 }
