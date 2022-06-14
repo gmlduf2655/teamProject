b0a45ed67fe914bc49194535f5aed3e6dfa473b6
gmlduf2655 <gmlduf2655@naver.com>
2022-06-14 11:06:15 +0900

    임희열
    1. 팔로우 기능 구현
     - mypage.jsp 수정
     - follow mapper, dao, service, controller 수정
     - follow dao, service 수정된 부분 테스트
    
    2. 메세지 페이징 및 다중 삭제 기능 구현
     - message mapper, dao, service, controller 수정
      - message_list.jsp 수정
      - message dao, service 수정된 부분 테스트

--- team_project/src/main/java/com/kh/team/dao/MessageDao.java
+++ team_project/src/main/java/com/kh/team/dao/MessageDao.java
@@ -3,6 +3,7 @@ package com.kh.team.dao;
 import java.util.List;
 
 import com.kh.team.vo.MessageVo;
+import com.kh.team.vo.PagingDto;
 
 public interface MessageDao {
 	// 메세지 내역 추가
@@ -10,9 +11,9 @@ public interface MessageDao {
 	// 답장 메세지 추가
 	public boolean insertReplyMessage(MessageVo messageVo);
 	// 보내는 메세지 조회
-	public List<MessageVo> selectSenderMessageList(String sender);
+	public List<MessageVo> selectSenderMessageList(String sender, PagingDto pagingDto);
 	// 받는 메세지 조회
-	public List<MessageVo> selectReceiverMessageList(String receiver);
+	public List<MessageVo> selectReceiverMessageList(String receiver, PagingDto pagingDto);
 	// 보내는 메세지 수 조회
 	public int selectSenderMessageCount(String sender);
 	// 받는 메세지 수 조회
@@ -27,6 +28,10 @@ public interface MessageDao {
 	public boolean updateReadDate(int messageno);
 	// 보내는 이가 메세지 내역 삭제
 	public boolean deleteMessageBySender(int messageno);
+	// 보내는 이 메세지 다중 삭제
+	public boolean deleteMultiMessageBySender(List<Integer> messagenos);
 	// 받는 이가 메세지 내역 삭제
 	public boolean deleteMessageByReceiver(int messageno);
+	// 받는 이 메세지 다중 삭제
+	public boolean deleteMultiMessageByReceiver(List<Integer> messagenos);
 }
